23f9d88b6b1b17fdbfa7d1082447023953719c25
Ahmed Abdullah <logames4freehack@gmail.com>
2024-09-13 18:32:21 +0300

    My config files for VoidLinux-Hyprland

---
+++ nyxt/auto-config.3.lisp
@@ -0,0 +1,16 @@
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration (web-buffer)
+  ((default-modes (pushnew 'nyxt/mode/style:dark-mode %slot-value%))))
+
+(define-configuration (web-buffer)
+  ((default-modes
+    (remove-if (lambda (nyxt::m) (string= (symbol-name nyxt::m) "DARK-MODE"))
+               %slot-value%))))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
+
+(define-configuration browser
+  ((theme theme:+dark-theme+)))
